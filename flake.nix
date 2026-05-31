dfaee2150b9ca33dc8913d68089abb67cede8a64
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2026-05-30 21:12:02 -0500

    Add my fork of xlaserpointer (directly in flake.nix)

--- flake.nix
+++ flake.nix
@@ -9,5 +9,18 @@
 
     packages.x86_64-linux.hello-imperium = nixpkgs.legacyPackages.x86_64-linux.hello;
 
+    packages.x86_64-linux.xlaserpointer = nixpkgs.legacyPackages.x86_64-linux.stdenv.mkDerivation rec {
+      pname = "xlaserpointer";
+      version = "1.0.0";
+      src = nixpkgs.legacyPackages.x86_64-linux.fetchFromGitHub {
+        owner = "PartemImperium";
+        repo = "xlaserpointer";
+        rev = "v${version}"; 
+        hash = "sha256-PXArDTeeQ4PyUviT6t4LEDM+LoEwj9Gy/x3qV+KS428="; 
+      };
+
+      # Tools needed during build time (like cmake)
+      nativeBuildInputs = with nixpkgs.legacyPackages.x86_64-linux;[ cmake pkg-config xorg.libX11 xorg.libXfixes xorg.libXi cairo ];
+    };
   };
 }
