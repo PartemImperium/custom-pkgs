ec3d3c6d3417eaa0c6abb91120d9501195e28e7c
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2026-06-16 00:15:44 -0500

    I think this is working????

--- pkgs/tallyarbiter/default.nix
+++ pkgs/tallyarbiter/default.nix
@@ -4,6 +4,9 @@
   fetchFromGitHub,
   buildNpmPackage,
 }:
+let
+  nodeVersion = pkgs.nodejs_26;
+in
 buildNpmPackage (finalAttrs: rec {
   pname = "tallyarbiter";
   version = "3.2.0";
@@ -15,6 +18,8 @@ buildNpmPackage (finalAttrs: rec {
     hash = "sha256-P3yNS3BQ2vnU/UEmNlSopiGtakXPJAk+LFhnuLTMT50="; 
   };
 
+  nodejs = nodeVersion;
+
   npmDepsHash = "sha256-ggGaIkbd5uotuttTvAsQhFElgadGhFvU+JCtsbGTnbw=";
 
   # We dont need electron bin from npm. 
@@ -24,18 +29,47 @@ buildNpmPackage (finalAttrs: rec {
   ui = buildNpmPackage rec {
     pname = "tallyarbiter-ui";
     inherit (finalAttrs) version src;
+    
+    nodejs = nodeVersion;
 
-    npmDepsHash = "sha256-ggGaIkbd5uotuttTvAsQhFElgadGhFvU+JCtsbGTnbw=";
+    sourceRoot = "${finalAttrs.src.name}/UI";
+
+    npmDepsHash = "sha256-KWK915JtJKUM5+t+Ew/LzahawDToH6Pc73vMxTkzfTQ=";
+
+    # We patches the version creation script to look at these env vars.
+    env.APP_VERSION = "${version}";
+    env.APP_BRANCH = "${version}";
+    env.APP_REMOTE = "${finalAttrs.src.url}";
+
+    #TODO: Figure out how to use the redundancyjs cli like it is normally and not just cp...
+    prePatch = ''
+      cp ../src/_models/* src/app/_models
+    '';
+
+    patches = [
+      ./patches/ui/dist.diff
+      ./patches/ui/version.diff
+      ./patches/ui/redundancy.diff
+    ];
+
     env.ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
 
+    installPhase = ''
+      runHook preInstall
+
+      cp -r dist $out
+
+      runHook postInstall
+    '';
   };
 
-  preInstall = ''
-    mkdir -p $out/bin/ui
-    cp -r ${ui}/bin $out/bin/ui
-    cp -r ${ui}/lib $out/bin/ui
-
-    
+  # Copy out dist from the UI build
+  preBuild = ''
+    cp -r ${ui} dist_ui
   '';
-  nodejs = pkgs.nodejs_26;
+
+  patches = [
+    ./patches/bin.diff
+  ];
+
 })
