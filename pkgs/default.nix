73e8a4cc2b933486a8195a3c32ca9af31b45c2f6
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2026-06-30 21:52:38 -0500

    Add gpo listener

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,4 +4,5 @@ rec {
     xlaserpointer = pkgs.callPackage ./xlaserpointer/default.nix {};
     plex-dbrepair = pkgs.callPackage ./plex-dbrepair/default.nix {};
     tallyarbiter = pkgs.callPackage ./tallyarbiter/default.nix {};
+    tallyarbiter-gpo-listener = pkgs.callPackage ./tallyarbiter/listeners/gpo.nix {};
 }
--- pkgs/tallyarbiter/default.nix
+++ pkgs/tallyarbiter/default.nix
@@ -1,11 +1,12 @@
 {
   pkgs,
+  lib,
   stdenv,
   fetchFromGitHub,
   buildNpmPackage,
 }:
 let
-  nodeVersion = pkgs.nodejs_26;
+  nodeVersion = pkgs.nodejs_26;  
 in
 buildNpmPackage (finalAttrs: rec {
   pname = "tallyarbiter";
@@ -64,12 +65,21 @@ buildNpmPackage (finalAttrs: rec {
   };
 
   # Copy out dist from the UI build
-  preBuild = ''
+  preInstall = ''
+    
     cp -r ${ui} dist_ui
   '';
 
   patches = [
     ./patches/bin.diff
   ];
-
+#https://github.com/NixOS/nixpkgs/blob/ed162fff81c9f93d06d89c93ef901dee2765ac1d/pkgs/by-name/lx/lx-music-desktop/package.nix
+# ^^^^^^^^^^^^^^^^^^ Example of electron app  
+meta = {
+    description = "The flexible and customizable camera tally light system";
+    longDescription = "A camera tally lights system capable of listening to and aggregating tally data from multiple sources and video switchers, then arbitrating Preview and Program states for connected devices.";
+    homepage = "https://www.tallyarbiter.com";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+  };
 })
