61a717675951662b3ddad8f7643d146b67bad5df
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2026-08-15 22:38:47 -0500

    helps if ya import the module...

--- modules/os/default.nix
+++ modules/os/default.nix
@@ -10,5 +10,6 @@
     #TODO: Create import function that takes a dir and imports the default.nix and the corrisponding os default file (default.linux.nix or default.darwin.nixs)
     
     ./defaults/localization/default.nix
+    ./defaults/nix-exprerimental-features/default.nix
   ];
 }
