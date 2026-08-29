3dc7b9d4dbd8bad7fbc6f99e1a778ba776fe7f58
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2026-08-29 17:19:35 -0500

    Turns out you cant use mkDefault on a option already set with mkDefault. Who knew?

--- modules/os/defaults/zsh/default.linux.nix
+++ modules/os/defaults/zsh/default.linux.nix
@@ -8,6 +8,6 @@
 with lib;
 {
   config = mkIf pkgs.stdenv.isLinux {
-    users.defaultUserShell = lib.mkDefault pkgs.zsh;
+    users.defaultUserShell = lib.mkOverride 900 pkgs.zsh;#TODO: Make a mkOverride helper that defaults the val to 900 to be a override for normal default but still overrideable by normal set.
   };
 }
